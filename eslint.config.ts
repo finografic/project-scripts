f108c70aa4028f58440bb89bd75efc54a3c8ac91
Justin <justin.blair.rankin@gmail.com>
2025-04-21 01:30:39 +0200

    fix..

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,24 @@
 import { ERROR, fino, INCLUDE_FILES_TS, OFF } from '@finografic/eslint-config';
 
-export default fino({
-  typescript: true,
-  gitignore: true,
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  overrides: {
-    jsonc: {
-      'jsonc/sort-keys': ERROR,
+export default fino(
+  {
+    typescript: true,
+    gitignore: true,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    overrides: {
+      jsonc: {
+        'jsonc/sort-keys': ERROR,
+      },
+    },
+    rules: {
+      'node/prefer-global/process': OFF,
+      'lint/complexity/noForEach': OFF,
     },
   },
-  rules: {
-    'node/prefer-global/process': OFF,
+  {
+    files: [...INCLUDE_FILES_TS],
   },
-}, {
-  files: [...INCLUDE_FILES_TS],
-});
+);
